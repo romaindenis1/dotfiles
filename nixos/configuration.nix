8763719a163d34ad7654d0e338327cd1e25c6d4a
Romain Denis <rom.denis.oz@gmail.com>
2025-06-11 20:16:41 +0000

    add(even more base files)

---
+++ nixos/configuration.nix
@@ -0,0 +1,68 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  users.users.r = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "audio" ];
+    shell = pkgs.zsh;
+  };
+
+  # Enable zsh program support
+  programs.zsh.enable = true;
+
+  # Enable Hyprland
+  programs.hyprland.enable = true;
+
+  # Greetd to launch Hyprland at login
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "Hyprland";
+        user = "r";
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    hyprland
+    waybar
+    kitty
+    neovim
+    wl-clipboard
+    git
+    gh
+    firefox
+    zsh
+    neofetch
+    stow
+  ];
+
+  # Networking
+  networking.networkmanager.enable = true;
+
+  networking.hostName = "nix";
+  # Bootloader - adjust this to your system type
+
+  # If you have BIOS/MBR:
+  boot.loader.grub = {
+    enable = true;
+    devices = [ "/dev/sda" ];  # replace /dev/sda with your boot drive
+  };
+
+  # If you have UEFI and want systemd-boot instead, comment out the above and uncomment:
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable ssh for convenience
+  services.openssh.enable = true;
+
+  # Locale and timezone
+  time.timeZone = "UTC";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  system.stateVersion = "25.05";
+}
+
