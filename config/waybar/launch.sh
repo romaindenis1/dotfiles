64887c9055d704b689e4d8dd9cc3f284fab3b28c
r <ps04egl@eduvaud.ch>
2025-12-14 14:53:37 +0000

    things

--- config/waybar/launch.sh
+++ config/waybar/launch.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-waybar -c ~/.config/waybar/config-custom -s ~/.config/waybar/style-custom.css > /dev/null 2>&1 &
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pkill -q waybar
+
+while pgrep -x waybar >/dev/null; do sleep 1; done
+
+waybar -c "$SCRIPT_DIR/config.jsonc" -s "$SCRIPT_DIR/style.css" &
